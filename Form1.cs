ead05636390c6d2b2b321549fdacaf2766e30c08
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-11-25 19:57:48 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+namespace Form1
+{
+    public partial class MainForm : Form
+    {
+        private DateTime targetDateTime;
+        private Timer timer; 
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+           
+            targetDateTime = new DateTime(2023, 12, 31, 23, 59, 59);
+
+            // Инициализация таймера
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            // Первоначальное обновление времени
+            UpdateTime();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            
+            TimeSpan remainingTime = targetDateTime - DateTime.Now;
+
+            
+            labelTimeRemaining.Text = $"{remainingTime.Days} дней {remainingTime.Hours} часов {remainingTime.Minutes} минут {remainingTime.Seconds} секунд";
+
+           
+            if (remainingTime.TotalSeconds <= 0)
+            {
+                labelTimeRemaining.Text = "Отсчет завершен!";
+                timer.Stop(); 
+            }
+        }
+    }
+}
